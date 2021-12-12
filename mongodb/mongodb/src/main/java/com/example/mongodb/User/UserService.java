8295e0f6546ed0a8b8559ebdb954b0e9edf0d671
Alex Ditto <49873977+alexditto@users.noreply.github.com>
2021-12-11 21:02:15 -0600

    Final commit for the night

--- mongodb/mongodb/src/main/java/com/example/mongodb/User/UserService.java
+++ mongodb/mongodb/src/main/java/com/example/mongodb/User/UserService.java
@@ -1,5 +1,19 @@
 package com.example.mongodb.User;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
-    
+    public List<User> getUsers(){
+        return List.of(
+            new User(
+                    (long) 1,
+                    "Fonix",
+                    "email@email.com",
+                    LocalDate.now()
+                ));
+    }
 }
