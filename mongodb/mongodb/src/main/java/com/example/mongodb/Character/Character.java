e811e2b8e510a435efad8933be30781e7e0892b6
Alex Ditto <49873977+alexditto@users.noreply.github.com>
2021-12-14 22:14:36 -0600

    Character class made

--- mongodb/mongodb/src/main/java/com/example/mongodb/Character/Character.java
+++ mongodb/mongodb/src/main/java/com/example/mongodb/Character/Character.java
@@ -23,4 +23,33 @@ public class Character {
         this.name = name;
         this.type = type;
     }
+
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public int getUserId(){
+        return userId;
+    }
+    public void setUserId(int userId){
+        this.userId = userId;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getType(){
+        return type;
+    }
+    public void setType(String type){
+        this.type = type;
+    }
+
 }
